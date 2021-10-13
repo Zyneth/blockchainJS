d8f5bf031e023e68926a90870a02ddbf10e199b1
Zyneth <resit.guerkan@gmail.com>
2021-10-13 10:49:36 +0200

    added calculateHash() function

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-class Block{
-    constructor(id, timestamp, data, previousHash =''){
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(id, timestamp, data, previousHash = '') {
         this.id = id;
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        return SHA256(this.id + this.previousHash + this.timestamp, +JSON.stringify(this.data)).toString()
     }
 }
