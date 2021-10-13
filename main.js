b0523e63325276ed789b4e63f00f80654c9b9f90
Zyneth <resit.guerkan@gmail.com>
2021-10-13 11:02:40 +0200

    added class Blockchain, testing with add() function

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const SHA256 = require('crypto-js/sha256');
+const SHA256 = require('crypto-js/sha256'); // SHA256 encryption method
 
 class Block {
-    constructor(id, timestamp, data, previousHash = '') {
+    constructor(id, timestamp, data, previousHash = '') { // standard constructor
         this.id = id;
         this.timestamp = timestamp;
         this.data = data;
@@ -13,3 +13,31 @@ class Block {
         return SHA256(this.id + this.previousHash + this.timestamp, +JSON.stringify(this.data)).toString()
     }
 }
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, '01/01/2016', 'Genesis Block', '0'); // the init block is called Genesis Block
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+
+    }
+}
+
+let uwuCoin = new Blockchain();
+uwuCoin.addBlock(new Block(1, '24/06/2014'), { amount: 2 });
+uwuCoin.addBlock(new Block(2, '21/11/2015'), { amount: 4 });
+
+console.log(JSON.stringify(uwuCoin, null, 4));
